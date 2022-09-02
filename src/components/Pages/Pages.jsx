6dfc2e7a4077951245d12a8f36afdbe9d8f07336
David Bécu <david.macnimo@outloock.fr>
2022-09-02 09:04:31 +0200

    Initial commit E1

---
+++ src/components/Pages/Pages.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Pages = () => {
+  return (
+    <>
+        <header  className='container'>
+            <div className='row bg-dark text-light text-center'>
+                <h1>La boutique de nom/prénom</h1>
+            </div>
+        </header>
+    </>
+  )
+}
+
+export default Pages

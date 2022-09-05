841b846578c1bfb0ee179e13a1eba38a5decadb2
David Bécu <david.macnimo@outloock.fr>
2022-09-05 12:47:09 +0200

    initial commit E6

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
+import { useState } from 'react';
 import './App.css';
 import Pages from './components/Pages/Pages';
 
 function App() {
+
   return (
     <>
+      <div>
         <Pages />
-
+      </div>
     </>
   );
 }
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './ContactForm.css';
 import { BsFillPersonFill } from "react-icons/bs";
 import { BsFillEnvelopeOpenFill } from "react-icons/bs";
@@ -8,42 +7,60 @@ import { BsFillPenFill } from "react-icons/bs";
 
 
 const ContactForm = () => {
-    return (
+
+    // <formObj nameObj={e.nameObj.target.value} />
+    
+    
+function formSubmit(event){
+    event.preventDefault();
+    const form =  event.target;
+    const formData = new FormData(form);
+    const jsonData = Object.fromEntries(formData.entries());
+    // version en une ligne
+    // const jsonData = Object.fromEntries(new FormData(event.target));
+    console.log(jsonData);
+};
+
+    
+return (
         <>
-            <form className='ContactForm d-flex align-items-end bg-dark text-light p-4'>
+            <form className='ContactForm d-flex align-items-end bg-dark text-light p-4' onSubmit={formSubmit}
+                     >
+                                        
                 <div className='divInputFormControl col-4 mb-1'>
                     <div className='divInput d-flex m-1'>
                         <div className="icon-xl me-2">
                             <BsFillPersonFill />
                         </div>
-                        <input className="form-control" type="text" placeholder="Nom, prénom" aria-label=".form-control" />
+                        <input className="form-control obj" type="text" name='nameObj' placeholder="Nom, prénom" aria-label=".form-control " require="true" />
                     </div>
                     <div className='divInput d-flex m-1'>
                         <div className="icon-xl me-2">
                             <BsFillEnvelopeOpenFill />
                         </div>
-                        <input className="form-control" type="text" placeholder="adresse@email.com" aria-label="Saisie par défaut" />
+                        <input className="form-control obj" type="text" name='addressObj' placeholder="adresse@email.com" aria-label="Saisie par défaut" require="true"/>
                     </div>
                     <div className='divInput d-flex m-1'>
                         <div className="icon-xl me-2">
                             < BsChatRightTextFill />
                         </div>
-                        <input className="form-control" type="text" placeholder="Sujet" aria-label=".form-control" />
+                        <input className="form-control obj" type="text" name='subjectObj' placeholder="Sujet" aria-label=".form-control" require="true"/>
                     </div>
                 </div>
-                <div class="divTextAreaFormControl d-flex col-8">
-                    <label for="FormControlTextArea" className="form-label ">Contactez-nous ...</label>
+                <div className="divTextAreaFormControl d-flex col-8">
+                    <label htmlFor="FormControlTextArea" className="form-label ">Contactez-nous ...</label>
                     <div className='d-flex m-1 '>
                         <div className="icon-xxl m-2">
                             < BsFillPenFill />
                         </div>
-                        <textarea className="form-control " id="FormControlTextArea" rows="3"></textarea>
+                        <textarea className="form-control obj" name='textObj' id="FormControlTextArea" rows="3" require="true"></textarea>
                     </div>
                     <div className="inputButton d-flex justify-content-end mt-1">
                         <input className='btn btn-primary ' type="submit" value="Envoyer" />
                     </div>
                 </div>
             </form>
+
         </>
     )
 }

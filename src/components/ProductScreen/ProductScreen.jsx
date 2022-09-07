a02b53e88e8219152fdff93effe1afdd03acde6b
David Bécu <david.macnimo@outloock.fr>
2022-09-07 10:43:42 +0200

    Initial commit E14

---
+++ src/components/ProductScreen/ProductScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductScreen = () => {
+  return (
+    <div className='ProductScreen'>
+        <h1>Product Screen</h1>
+    </div>
+  )
+}
+
+export default ProductScreen
